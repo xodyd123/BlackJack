84573c434b95e65a14b31310a08a4c3f146fd991
xodyd123 <gksrmf9186@naver.com>
2025-12-08 11:38:30 +0900

    feat(player): hit 메서드 추가

--- src/main/kotlin/com/taeyong/blackjack/domain/player/Player.kt
+++ src/main/kotlin/com/taeyong/blackjack/domain/player/Player.kt
@@ -1,6 +1,7 @@
 package com.taeyong.blackjack.domain.player
 
 import com.taeyong.blackjack.domain.card.Card
+import com.taeyong.blackjack.domain.deck.Deck
 import com.taeyong.blackjack.domain.hand.Hand
 
 class Player(private val hand: Hand) {
@@ -14,4 +15,10 @@ class Player(private val hand: Hand) {
     fun receive(card: Card) {
         hand.add(card)
     }
+
+    fun hit(deck: Deck){
+        hand.add(deck.draw())
+    }
+
+
 }
--- src/test/kotlin/com/taeyong/blackjack/domain/player/PlayerTest.kt
+++ src/test/kotlin/com/taeyong/blackjack/domain/player/PlayerTest.kt
@@ -3,6 +3,7 @@ package com.taeyong.blackjack.domain.player
 import com.taeyong.blackjack.domain.card.Card
 import com.taeyong.blackjack.domain.card.Rank
 import com.taeyong.blackjack.domain.card.Suit
+import com.taeyong.blackjack.domain.deck.FakeDeck
 import com.taeyong.blackjack.domain.hand.Hand
 import com.taeyong.blackjack.domain.score.ScoreCalculator
 import kotlin.test.Test
@@ -63,4 +64,30 @@ class PlayerTest {
 
         assertFalse { player.isBust }
     }
+
+    @Test
+    fun `플레이어는_HIT_를_하면_덱에서_카드를_뽑아_핸드에_추가한다`() {
+        val hand = Hand(ScoreCalculator())
+        val player = Player(hand)
+        val deck = FakeDeck(listOf(Card(Rank.J, Suit.SPADE), Card(Rank.FOUR, Suit.HEART)))
+        player.hit(deck)
+
+        assertEquals(1, hand.size)
+        assertEquals(10, hand.score)
+        assertEquals(1, deck.drawCount)
+    }
+
+    @Test
+    fun `플레이어는_HIT_를_여러번_하면_카드가_누적된다`() {
+        val hand = Hand(ScoreCalculator())
+        val player = Player(hand)
+        val deck = FakeDeck(listOf(Card(Rank.J, Suit.SPADE), Card(Rank.FOUR, Suit.HEART)))
+        player.hit(deck)
+        player.hit(deck)
+
+        assertEquals(2, hand.size)
+        assertEquals(2, deck.drawCount)
+        assertEquals(0, deck.size())
+    }
+
 }
